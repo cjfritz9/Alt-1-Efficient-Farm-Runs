3a3fdd29cbb72b5c5e873d2e0c2f0c2f86b0127b
CJ <cjfritz9@gmail.com>
2022-12-06 17:45:54 -0700

    css and states for presets menu

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import UserPresets, { Preset } from '../models/preset';
+import UserPresets from '../models/preset';
 import Profile from '../models/profile';
 
 const Home: React.FC = () => {
   const [presetData, setPresetData] = useState<UserPresets>();
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [deleteMode, setDeleteMode] = useState<boolean>(false);
   const presetCountRef = useRef<number>(0);
+  // console.log('ED', editMode);
+  // console.log('DEL', deleteMode);
 
   const navigate = useNavigate();
 
@@ -14,7 +18,7 @@ const Home: React.FC = () => {
   };
 
   const setCasing = (patchType: string) => {
-    console.log(patchType);
+    // console.log(patchType);
     if (patchType === 'fruit-tree') {
       return 'Fruit Tree';
     } else if (patchType.length) {
@@ -28,10 +32,40 @@ const Home: React.FC = () => {
     const data: Profile = JSON.parse(
       localStorage.getItem('efficient_farm_runs')!
     );
-    data.presets.preset2.type = 'bush';
-    data.presets.preset3.type = 'bush';
-    data.presets.preset4.type = 'bush';
+    data.presets.preset2.type = 'tree';
+    data.presets.preset3.type = '';
+    data.presets.preset4.type = '';
+    data.presets.preset5.type = '';
     localStorage.setItem('efficient_farm_runs', JSON.stringify(data));
+    window.location.reload();
+  };
+
+  const modeHandler = (option: string) => {
+    if (option === 'edit') {
+      if (deleteMode) {
+        setDeleteMode(false);
+      }
+      const editButton = document.getElementById('preset-edit-btn')!;
+      // editButton.classList.remove('');
+      if (editButton.classList.contains('button-selected')) {
+        editButton.classList.remove('button-selected');
+      } else {
+        editButton.classList.add('button-selected');
+      }
+      setEditMode((prevEditMode) => !prevEditMode);
+    } else {
+      if (editMode) {
+        setEditMode(false);
+      }
+      const delButton = document.getElementById('preset-del-btn')!;
+      // delButton.classList.remove('');
+      if (delButton.classList.contains('button-selected')) {
+        delButton.classList.remove('button-selected');
+      } else {
+        delButton.classList.add('button-selected');
+      }
+      setDeleteMode((prevDeleteMode) => !prevDeleteMode);
+    }
   };
 
   useEffect(() => {
@@ -42,7 +76,7 @@ const Home: React.FC = () => {
       presetCountRef.current = Object.values(parsedUserData.presets).filter(
         (preset) => preset.type.length
       ).length;
-      console.log(presetCountRef.current);
+      // console.log(presetCountRef.current);
     }
   }, []);
 
@@ -50,21 +84,45 @@ const Home: React.FC = () => {
     <main className='outer-wrapper'>
       {presetData ? (
         <>
-          <h2 className='preset-header'>load a preset</h2>
+          {editMode ? (
+            <h2 className='preset-header'>edit which preset?</h2>
+          ) : deleteMode ? (
+            <h2 className='preset-header'>delete which preset?</h2>
+          ) : (
+            <h2 className='preset-header'>Presets</h2>
+          )}
           {Object.values(presetData).map((preset, idx) => {
             return preset.type.length ? (
-              <button key={idx} id='preset-selection-btn' className='nis-button'>{`${setCasing(
-                preset.type
-              )} Run`}</button>
+              <button
+                key={idx}
+                id='preset-selection-btn'
+                className='nis-button'
+              >{`${setCasing(preset.type)} Run`}</button>
             ) : null;
           })}
           {presetCountRef.current < 5 ? (
-            <button id='presets-add-btn' className='nis-button nis-button-alt'>Add Another</button>
+            <button id='presets-add-btn' className='nis-button nis-button-alt'>
+              Add Another
+            </button>
           ) : null}
-          <h2 id='options-header' className='preset-header'>options</h2>
+          <h2 id='options-header' className='preset-header'>
+            options
+          </h2>
           <div id='presets-btn-wrapper' className='dbl-btn-wrapper'>
-            <button id='preset-selection-btn' className='nis-button nis-button-alt'>Edit</button>
-            <button id='preset-selection-btn' className='nis-button nis-button-alt'>Delete</button>
+            <button
+              id='preset-edit-btn'
+              className='nis-button edit-btn'
+              onClick={() => modeHandler('edit')}
+            >
+              Edit
+            </button>
+            <button
+              id='preset-del-btn'
+              className='nis-button del-btn'
+              onClick={() => modeHandler('delete')}
+            >
+              Delete
+            </button>
           </div>
         </>
       ) : (
