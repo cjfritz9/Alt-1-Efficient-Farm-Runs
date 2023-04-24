0bf7c54b88ddeaf560a17d2358692c4465554d0f
CJ <cjfritz9@gmail.com>
2023-04-24 13:23:16 -0600

    fixed some issues relating to outdated ts version and eslint

--- server/server.js
+++ server/server.js
@@ -1,43 +1,140 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+//@ts-nocheck
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
     }
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+    return new (P || (P = Promise))(function (resolve, reject) {
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator['throw'](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+  };
+var __generator =
+  (this && this.__generator) ||
+  function (thisArg, body) {
+    var _ = {
+        label: 0,
+        sent: function () {
+          if (t[0] & 1) throw t[1];
+          return t[1];
+        },
+        trys: [],
+        ops: []
+      },
+      f,
+      y,
+      t,
+      g;
+    return (
+      (g = { next: verb(0), throw: verb(1), return: verb(2) }),
+      typeof Symbol === 'function' &&
+        (g[Symbol.iterator] = function () {
+          return this;
+        }),
+      g
+    );
+    function verb(n) {
+      return function (v) {
+        return step([n, v]);
+      };
+    }
+    function step(op) {
+      if (f) throw new TypeError('Generator is already executing.');
+      while ((g && ((g = 0), op[0] && (_ = 0)), _))
+        try {
+          if (
+            ((f = 1),
+            y &&
+              (t =
+                op[0] & 2
+                  ? y['return']
+                  : op[0]
+                  ? y['throw'] || ((t = y['return']) && t.call(y), 0)
+                  : y.next) &&
+              !(t = t.call(y, op[1])).done)
+          )
+            return t;
+          if (((y = 0), t)) op = [op[0] & 2, t.value];
+          switch (op[0]) {
+            case 0:
+            case 1:
+              t = op;
+              break;
+            case 4:
+              _.label++;
+              return { value: op[1], done: false };
+            case 5:
+              _.label++;
+              y = op[1];
+              op = [0];
+              continue;
+            case 7:
+              op = _.ops.pop();
+              _.trys.pop();
+              continue;
+            default:
+              if (
+                !((t = _.trys), (t = t.length > 0 && t[t.length - 1])) &&
+                (op[0] === 6 || op[0] === 2)
+              ) {
+                _ = 0;
+                continue;
+              }
+              if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) {
+                _.label = op[1];
+                break;
+              }
+              if (op[0] === 6 && _.label < t[1]) {
+                _.label = t[1];
+                t = op;
+                break;
+              }
+              if (t && _.label < t[2]) {
+                _.label = t[2];
+                _.ops.push(op);
+                break;
+              }
+              if (t[2]) _.ops.pop();
+              _.trys.pop();
+              continue;
+          }
+          op = body.call(thisArg, _);
+        } catch (e) {
+          op = [6, e];
+          y = 0;
+        } finally {
+          f = t = 0;
+        }
+      if (op[0] & 5) throw op[1];
+      return { value: op[0] ? op[1] : void 0, done: true };
+    }
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.app = void 0;
-var axios_1 = require("axios");
+var axios_1 = require('axios');
 require('dotenv').config();
 var express = require('express');
 var cors = require('cors');
@@ -45,48 +142,64 @@ exports.app = express();
 var PORT = process.env.PORT || 8080;
 exports.app.use(express.json());
 exports.app.use(cors({ origin: '*' }));
-exports.app.get('/api/rs3/skills/:username', function (req, res) { return __awaiter(void 0, void 0, void 0, function () {
+exports.app.get('/api/rs3/skills/:username', function (req, res) {
+  return __awaiter(void 0, void 0, void 0, function () {
     var username;
     return __generator(this, function (_a) {
-        switch (_a.label) {
-            case 0:
-                username = req.params.username;
-                console.log('username: ', username);
-                return [4 /*yield*/, axios_1.default
-                        .get("https://apps.runescape.com/runemetrics/profile/profile?user=".concat(encodeURI(username), "&activities=0"), {
-                        headers: {
-                            'content-type': 'Application/json',
-                            'accept-encoding': '*'
-                        }
-                    })
-                        .then(function (response) {
-                        console.log(response.data);
-                        if (response.data.error)
-                            return res.send(response.data);
-                        var skills = response.data.skillvalues;
-                        var hasQuestCape = response.data.questsnotstarted === 0 &&
-                            response.data.questsstarted === 0
-                            ? true
-                            : false;
-                        console.log('tq: ', hasQuestCape, response.data.questscomplete);
-                        var desiredSkills = skills.filter(function (skill) {
-                            return skill.id === 19 || skill.id === 6;
-                        });
-                        desiredSkills.forEach(function (skill) {
-                            skill.skill = skill.id === 19 ? 'Farming' : 'Magic';
-                        });
-                        res.send({ name: response.data.name, hasQuestCape: hasQuestCape, levels: desiredSkills });
-                    })
-                        .catch(function (err) {
-                        console.error('error: ', err);
-                        res.send({ error: 'Unknown error, try again' });
-                    })];
-            case 1:
-                _a.sent();
-                return [2 /*return*/];
-        }
+      // eslint-disable-next-line default-case
+      switch (_a.label) {
+        case 0:
+          username = req.params.username;
+          console.log('username: ', username);
+          return [
+            4 /*yield*/,
+            axios_1.default
+              .get(
+                'https://apps.runescape.com/runemetrics/profile/profile?user='.concat(
+                  encodeURI(username),
+                  '&activities=0'
+                ),
+                {
+                  headers: {
+                    'content-type': 'Application/json',
+                    'accept-encoding': '*'
+                  }
+                }
+              )
+              .then(function (response) {
+                console.log(response.data);
+                if (response.data.error) return res.send(response.data);
+                var skills = response.data.skillvalues;
+                var hasQuestCape =
+                  response.data.questsnotstarted === 0 &&
+                  response.data.questsstarted === 0
+                    ? true
+                    : false;
+                console.log('tq: ', hasQuestCape, response.data.questscomplete);
+                var desiredSkills = skills.filter(function (skill) {
+                  return skill.id === 19 || skill.id === 6;
+                });
+                desiredSkills.forEach(function (skill) {
+                  skill.skill = skill.id === 19 ? 'Farming' : 'Magic';
+                });
+                res.send({
+                  name: response.data.name,
+                  hasQuestCape: hasQuestCape,
+                  levels: desiredSkills
+                });
+              })
+              .catch(function (err) {
+                console.error('error: ', err);
+                res.send({ error: 'Unknown error, try again' });
+              })
+          ];
+        case 1:
+          _a.sent();
+          return [2 /*return*/];
+      }
     });
-}); });
+  });
+});
 exports.app.listen(PORT, function () {
-    console.log("App listening on port ".concat(PORT));
+  console.log('App listening on port '.concat(PORT));
 });
